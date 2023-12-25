b44ab8b7f0b5f0c8a1062f17099537b5fdc7c137
andreili <andreil499@gmail.com>
2023-12-25 20:57:25 +0100

    Add all graphical modes.

--- vrf/gui_output.hpp
+++ vrf/gui_output.hpp
@@ -69,16 +69,22 @@ public:
         this->p_fc = (port_fc_u*)p_fc;
     }
 
-    void draw_mem(std::function<void(int,int,uint8_t,uint8_t,uint8_t,uint8_t)> set_cb)
+    #define RGB(b,g,r) (((r) << 16) | ((g) << 8) | (b))
+
+    void draw_mem(std::function<void(int,int,uint32_t)> set_cb)
     {
-        uint32_t scr_no;
-        if (p_f8->bt.mode & (1 << 4))
+        if ((this->p_f8->bt.mode == 2) || (this->p_f8->bt.mode == 3))
         {
-            scr_no = p_fa->bt.scr_no | 1;
+            return;
+        }
+        uint32_t scr_no;
+        if (this->p_f8->bt.mode & (1 << 4))
+        {
+            scr_no = this->p_fa->bt.scr_no | 1;
         }
         else
         {
-            scr_no = p_fa->bt.scr_no;
+            scr_no = this->p_fa->bt.scr_no;
         }
         uintptr_t scr_start_addr;
         switch (scr_no)
@@ -97,24 +103,105 @@ public:
         scr_plane_1 = scr_start_addr + 0x04000;
         scr_plane_2 = scr_start_addr + 0x10000;
         scr_plane_3 = scr_start_addr + 0x14000;
+        uint32_t c0, c1, c2, c3;
+        switch (this->p_f8->bt.mode)
+        {
+        case 0: c0 = RGB(0x00, 0x00, 0x00); c1 = RGB(0x00, 0xff, 0x00);
+                break;
+        case 1: c0 = RGB(0xc8, 0xb4, 0x28); c1 = RGB(0x32, 0xfa, 0xfa);
+                break;
+        case 4: c0 = RGB(0x00, 0x00, 0x00); c1 = RGB(0x00, 0x00, 0xc0); c2 = RGB(0x00, 0xc0, 0x00); c3 = RGB(0xc0, 0x00, 0x00);
+                break;
+        case 5: c0 = RGB(0xc0, 0xc0, 0xc0); c1 = RGB(0x00, 0x00, 0xc0); c2 = RGB(0x00, 0xc0, 0x00); c3 = RGB(0xc0, 0x00, 0x00);
+                break;
+        }
         for (int x=0 ; x<this->width ; x+=8)
         {
-            uintptr_t line_addr = 0xc000 + ((x >> 3) << 8);
             for (int y=0 ; y<this->height ; ++y)
             {
-                uintptr_t col_addr = line_addr | y;
-                uint8_t pixels = this->p_memory[col_addr];
+                uint8_t pixels0 = this->p_memory[(scr_plane_0 + ((x >> 3) << 8)) | y];
+                uint8_t pixels1 = this->p_memory[(scr_plane_1 + ((x >> 3) << 8)) | y];
+                uint8_t pixels2 = this->p_memory[(scr_plane_2 + ((x >> 3) << 8)) | y];
+                uint8_t pixels3 = this->p_memory[(scr_plane_3 + ((x >> 3) << 8)) | y];
+                if ((this->p_f8->bt.mode == 14) || (this->p_f8->bt.mode == 15))
+                {
+                    pixels2 = this->p_fc->b;
+                }
+                if ((this->p_f8->bt.mode == 6) || (this->p_f8->bt.mode == 7) ||
+                    (this->p_f8->bt.mode == 14) || (this->p_f8->bt.mode == 15))
+                {
+                    uint32_t ci, cr, cg, cb;
+                    ci = (pixels2 & (1 << 7)) >> 1;
+                    cr = (pixels2 & (1 << 6)) ? (ci | 0xbf) : 0x00;
+                    cg = (pixels2 & (1 << 5)) ? (ci | 0xbf) : 0x00;
+                    cb = (pixels2 & (1 << 4)) ? (ci | 0xbf) : 0x00;
+                    c0 = RGB(cb, cg, cr);
+                    ci = (pixels2 & (1 << 3)) << 3;
+                    cr = (pixels2 & (1 << 2)) ? (ci | 0xbf) : 0x00;
+                    cg = (pixels2 & (1 << 1)) ? (ci | 0xbf) : 0x00;
+                    cb = (pixels2 & (1 << 0)) ? (ci | 0xbf) : 0x00;
+                    c1 = RGB(cb, cg, cr);
+                }
                 for (int i=0 ; i<8 ; ++i)
                 {
-                    if (pixels & (1 << 7))
+                    uint8_t pxl0 = (pixels0 >> 7);
+                    uint8_t pxl1 = (pixels1 >> 7);
+                    uint8_t pxl2 = (pixels2 >> 7);
+                    uint8_t pxl3 = (pixels3 >> 7);
+                    switch (this->p_f8->bt.mode)
                     {
-                        set_cb(x+i, y, 0, 0xff, 0, 0);
+                    case 0:
+                    case 1:
+                    case 6:
+                    case 7:
+                    case 14:
+                    case 15:
+                        if (pxl0)
+                        {
+                            set_cb(x+i, y, c1);
+                        }
+                        else
+                        {
+                            set_cb(x+i, y, c0);
+                        }
+                        break;
+                    case 4:
+                    case 5:
+                        switch ((pxl0 << 1) | pxl2)
+                        {
+                        case 0: set_cb(x+i, y, c0);
+                                break;
+                        case 1: set_cb(x+i, y, c1);
+                                break;
+                        case 2: set_cb(x+i, y, c2);
+                                break;
+                        default:set_cb(x+i, y, c3);
+                                break;
+                        }
+                        break;
                     }
-                    else
+                    switch (this->p_f8->bt.mode & 0x14)
                     {
-                        set_cb(x+i, y, 0, 0, 0, 0);
+                    case 0x10:
+                        c0 = 0;
+                        if (pxl0) { c0 |= RGB(0x00, 0x00, 0xbf); }
+                        if (pxl1) { c0 |= RGB(0x00, 0xbf, 0x00); }
+                        if (pxl2) { c0 |= RGB(0xbf, 0x00, 0x00); }
+                        set_cb(x+i, y, c0);
+                        break;
+                    case 0x14:
+                        c0 = 0;
+                        if (pxl0) { c0 |= RGB(0x00, 0x00, 0xbf); }
+                        if (pxl1) { c0 |= RGB(0x00, 0xbf, 0x00); }
+                        if (pxl2) { c0 |= RGB(0xbf, 0x00, 0x00); }
+                        if (pxl3) { c0 |= RGB(0x40, 0x40, 0x40); }
+                        set_cb(x+i, y, c0);
+                        break;
                     }
-                    pixels <<= 1;
+                    pixels0 <<= 1;
+                    pixels1 <<= 1;
+                    pixels2 <<= 1;
+                    pixels3 <<= 1;
                 }
             }
         }
@@ -130,10 +217,9 @@ public:
         background_image.fill(0xffffffff);
 
         QPainter painter(&background_image);
-        draw_mem([this](int x, int y, uint8_t r, uint8_t g, uint8_t b, uint8_t i)
+        draw_mem([this](int x, int y, uint32_t argb)
             {
-                uint32_t pxl = (r << 16) | (g << 8) | b;
-                this->p_image->setPixel(x, y, pxl);
+                this->p_image->setPixel(x, y, argb);
             });
         painter.drawImage(0, 0, *this->p_image);
 
